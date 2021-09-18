e2341322ea975005c3d76f980b73ff95fbe8b618
Anton Ždanov <azdanov@pm.me>
2021-09-18 22:26:12 +0300

    add about page

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "To contact me, please send an email to "+
+			"<a href=\"mailto:support@myphoto.com\">support@myphoto.com</a>")
+	}
 }
 
 func main() {
