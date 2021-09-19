9a187c8652354d868f7e144b12f607a7c13ccfcc
Anton Ždanov <azdanov@pm.me>
2021-09-19 14:13:26 +0300

    use gorilla/mux router

--- main.go
+++ main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "To contact me, please send an email to "+
-			"<a href=\"mailto:support@myphoto.com\">support@myphoto.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404: The page for "+r.URL.Path+" could not be found</h1>")
-	}
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To contact me, please send an email to "+
+		"<a href=\"mailto:support@myphoto.com\">support@myphoto.com</a>")
+}
+
+func error404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404: The page for "+r.URL.Path+" could not be found</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("localhost:3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/about", about)
+	r.NotFoundHandler = http.HandlerFunc(error404)
+	http.ListenAndServe("localhost:3000", r)
 }
