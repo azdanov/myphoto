b7b1cc64a9dc70740819c5f34f95c822098b2dc8
Anton Ždanov <azdanov@pm.me>
2021-09-21 05:59:42 +0300

    add signup page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
+	signupView  *views.View
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
@@ -22,13 +23,20 @@ func contact(w http.ResponseWriter, _ *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func main() {
 	homeView = views.NewView("index", "views/home.gohtml")
 	contactView = views.NewView("index", "views/contact.gohtml")
+	signupView = views.NewView("index", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	must(http.ListenAndServe("localhost:3000", r))
 }
 
