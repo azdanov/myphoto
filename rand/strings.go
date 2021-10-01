d7363460d071c63f46eb6d62492639cfc6f307a0
Anton Ždanov <azdanov@pm.me>
2021-10-01 21:46:39 +0300

    add rand package

---
+++ rand/strings.go
@@ -0,0 +1,33 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+const RememberTokenBytes = 32
+
+// Bytes generates n random bytes, or an error.
+// This uses crypto/rand and is safe to use with tokens.
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// String generate a base64 string of nBytes,
+// or an empty string and an error.
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+// RememberToken generates tokens of a predefined byte size.
+func RememberToken() (string, error) {
+	return String(RememberTokenBytes)
+}
