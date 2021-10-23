c5c24a57b16d1fd269c6fc6eef5572862d55bf09
Anton Ždanov <azdanov@pm.me>
2021-10-24 00:03:36 +0300

    add galleries model

---
+++ models/galleries.go
@@ -0,0 +1,22 @@
+package models
+
+import "gorm.io/gorm"
+
+// Gallery represents the image resources stored in the database.
+type Gallery struct {
+	gorm.Model
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface {
+	GalleryDB
+}
+
+type GalleryDB interface {
+	ByID(id uint) (*Gallery, error)
+	ByUserID(userID uint) ([]Gallery, error)
+	Create(gallery *Gallery) error
+	Update(gallery *Gallery) error
+	Delete(id uint) error
+}
